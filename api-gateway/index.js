cf7cba81afac629cf6cf03ec25c9c4c4c9c38de3
yosh escorcia <yosh.escorcia@gmail.com>
2025-12-17 12:01:29 -0600

    new healthcheck for healthGW1

--- api-gateway/index.js
+++ api-gateway/index.js
@@ -9,6 +9,15 @@ fastify.get('/healthgateway', async function () {
   };
 });
 
+fastify.get('/healthGW1', async function () {
+  return {
+    service: "Checking services v1...",
+    commit: process.env.DEPLOY_COMMIT,
+    status: 'ok',
+    timestamp: new Date().toISOString()
+  };
+});
+
 const port = process.env.PORT || 8080;
 fastify.listen({ port, host: '0.0.0.0' }, err => {
   if (err) {
