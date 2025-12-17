d20a7e59be2e2f5f1439934cc31e0082e04f82dd
yosh escorcia <yosh.escorcia@gmail.com>
2025-12-17 09:48:20 -0600

    initial commit

---
+++ api-gateway/index.js
@@ -0,0 +1,18 @@
+const fastify = require('fastify')({ logger: true });
+
+fastify.get('/healthgateway', async function () {
+  return {
+    service: process.env.SERVICE_NAME,
+    commit: process.env.DEPLOY_COMMIT,
+    status: 'ok',
+    timestamp: new Date().toISOString()
+  };
+});
+
+const port = process.env.PORT || 8080;
+fastify.listen({ port, host: '0.0.0.0' }, err => {
+  if (err) {
+    fastify.log.error(err);
+    process.exit(1);
+  }
+});
---
+++ brainyCore/index.js
@@ -0,0 +1,19 @@
+const fastify = require('fastify')({ logger: true });
+
+fastify.get('/healthbrainy', async function () {
+  return {
+    service: process.env.SERVICE_NAME,
+    commit: process.env.DEPLOY_COMMIT,
+    status: 'ok',
+    timestamp: new Date().toISOString()
+  };
+});
+
+const port = process.env.PORT || 8080;
+
+fastify.listen({ port, host: '0.0.0.0' }, err => {
+  if (err) {
+    fastify.log.error(err);
+    process.exit(1);
+  }
+});
